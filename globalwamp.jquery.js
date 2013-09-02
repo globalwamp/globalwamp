556f33708f9e9ebb8928d25d63fcec25ee421c34
osk <osk@li316-77.members.linode.com>
2013-09-02 01:04:14 -0300

    primeros intentos de edicion de marscadores

--- globalwamp.jquery.js
+++ globalwamp.jquery.js
@@ -11,6 +11,7 @@
     //Defaults:
     this.defaults = {
       source: '0AqdTbs1TYvZKdE10TFRsa1BISE50amVTeUVNUDVqNkE',
+      editable:false,
       vistaInicial: {
         lat: -34,
         lng: -59,
@@ -231,7 +232,7 @@
       var _this = this;
       var map_options = {};
 
-      $mapa = _this.$el;
+      var $mapa = _this.$el;
       if (_this.opts.vistaInicial.lat !== undefined) {
         map_options.center = [ _this.opts.vistaInicial.lat, _this.opts.vistaInicial.lng ];      
       }  
@@ -245,7 +246,6 @@
       if (_this.opts.vistaInicial.layer !== undefined) {
         $mapa.capaBase( _this.opts.vistaInicial.layer );      
       }
-
 
       /*
       $(_this.wms).each(function(k,layer) {
@@ -279,6 +279,16 @@
         });
 
       });
+      
+
+      if (_this.opts.editable) {
+        $mapa.data().Lmap.addControl(new L.Control.ViewCenter({$map: $mapa}));
+        $mapa.data().Lmap.addControl( new L.Control.Search({
+          layer: $mapa.data().plugin_leaflet.popupGroup,
+          initial: false}) );
+        $mapa.find('.bar').hide();
+        $mapa.enableMarkerDragging();
+      }
       
       $(_this.kml).each(function(k, kml) {
         $mapa.addKML({
